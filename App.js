c00a20baed330afdd1d80df27c8214a218fd465a
Sina <sina.dorostkar@chrysalisloyalty.com>
2021-12-27 18:42:37 +0000

    adding home screen and refactoring

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
-import cryptocurrencies from "./assets/data/cryptocurrencies.json";
-import CoinItem from "./src/components/CoinItem";
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={cryptocurrencies}
-        renderItem={({ item }) => <CoinItem marketCoin={item} />}
-      />
+      <HomeScreen />
       <StatusBar style="light" />
     </View>
   );
